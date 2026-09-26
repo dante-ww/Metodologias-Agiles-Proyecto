e4688fe45a9da00e94a9a7bd2d1211721f858680
Dante <dante.work2004@gmail.com>
2026-09-26 02:13:15 -0300

    feat: nuevo model para el get de usuarios

---
+++ Backend/Kiosco.Api/Models/UsuarioResponse.cs
@@ -0,0 +1,9 @@
+namespace Kiosco.Api.Models
+{
+    public class UsuarioResponse
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public string Rol { get; set; } = "";
+    }
+}
